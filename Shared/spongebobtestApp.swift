b731ae224aa20363a37fc599a25878540714e141
曹博鈞 <a131437422@gmail.com>
2021-11-29 22:04:38 +0800

    Initial Commit

---
+++ Shared/spongebobtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  spongebobtestApp.swift
+//  Shared
+//
+//  Created by Chang Jack on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct spongebobtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
